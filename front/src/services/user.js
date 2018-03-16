1c52d7b5ef08c1a766deed29f2a166275d73c333
Jerome Doucet <jerdct@gmail.com>
2018-03-16 21:33:24 +0100

    add logout and login function on user services

--- front/src/services/user.js
+++ front/src/services/user.js
@@ -4,3 +4,11 @@ import { SESSION_COOKIE } from '@/constants'
 export function isAuthenticated() {
   return Cookies.get(SESSION_COOKIE) !== undefined;
 }
+
+export function logout() {
+  Cookies.remove(SESSION_COOKIE);
+}
+
+export function login(token) {
+  Cookies.set(SESSION_COOKIE, token);
+}
--- front/tests/unit/services/user.spec.js
+++ front/tests/unit/services/user.spec.js
@@ -1,21 +1,45 @@
-import { isAuthenticated } from '@/services/user'
+import { 
+  isAuthenticated,
+  logout,
+  login
+} from '@/services/user'
 import { SESSION_COOKIE } from '@/constants'
 import Cookies from 'js-cookie';
 
-describe('isAuthenticated', () => {
+describe('user services', () => {
 
   beforeEach(() => {
     Cookies.remove(SESSION_COOKIE);
   });
 
-  it('return true if a session cookie is present', () => {
-    Cookies.set(SESSION_COOKIE, 'sometoken');
-    const authenticated = isAuthenticated();
-    expect(authenticated).toBeTruthy();
+  describe('isAuthenticated', () => {
+
+    it('return true if a session cookie is present', () => {
+      Cookies.set(SESSION_COOKIE, 'sometoken');
+      const authenticated = isAuthenticated();
+      expect(authenticated).toBeTruthy();
+    });
+
+    it('return false if a session cookie is not present', () => {
+      const authenticated = isAuthenticated();
+      expect(authenticated).toBeFalsy();
+    });
   });
 
-  it('return false if a session cookie is not present', () => {
-    const authenticated = isAuthenticated();
-    expect(authenticated).toBeFalsy();
+  describe('logout', () => {
+
+    it('remove session cookie', () => {
+      Cookies.set(SESSION_COOKIE, 'sometoken');
+      logout();
+      expect(Cookies.get(SESSION_COOKIE)).toBeUndefined();
+    });
+  });
+
+  describe('login', () => {
+    
+    it('set session cookie', () => {
+      login('someToken');
+      expect(Cookies.get(SESSION_COOKIE)).toBe('someToken');
+    })
   });
 });
