509b82a28b7998097f4eb3c81b5a17abfa611fee
Jerome Doucet <jerdct@gmail.com>
2018-07-01 11:10:47 +0200

    [fix] redirect on home pagin in navbar

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
-  <b-navbar toggleable="md" type="dark" variant="dark">
+  <b-navbar id='dahu-navbar' toggleable="md" type="dark" variant="dark">
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-    <b-navbar-brand href="#">Dahu</b-navbar-brand> 
+    <b-navbar-brand href="#/">Dahu</b-navbar-brand> 
     <b-navbar-nav class="ml-auto">
       <b-nav-form right v-if="isAuthenticated()">
         <b-button size="sm" class="my-2 my-sm-0" type="button" v-on:click="disconnect">Disconnect</b-button>
@@ -29,4 +29,7 @@ export default {
 </script>
 
 <style scoped>
+#dahu-navbar {
+  margin-bottom: 3rem;
+}
 </style>
