0fe640a9e47cc94c68a8d4720a75723f996e6400
Jerome Doucet <jerdct@gmail.com>
2018-07-26 21:05:18 +0200

    add createJob() function

--- front/src/requests/jobs.js
+++ front/src/requests/jobs.js
@@ -1,6 +1,15 @@
 import { handleResponse } from '@/requests/utils';
 import { getToken } from '@/requests/utils';
 
+export function createJob(job) {
+  return fetch('/jobs', {
+    method: "POST",
+    headers: { Authorization: getToken() },
+    body: JSON.stringify(job)
+  })
+    .then(handleResponse);
+}
+
 export function fetchJobs() {
   return fetch('/jobs', {
     method: "GET",
--- front/tests/unit/requests/jobs.spec.js
+++ front/tests/unit/requests/jobs.spec.js
@@ -1,5 +1,5 @@
 import fetchMock from "fetch-mock";
-import { fetchJobs } from '@/requests/jobs';
+import { fetchJobs, createJob } from '@/requests/jobs';
 import { SESSION_COOKIE } from '@/constants'
 import Cookies from 'js-cookie';
 
@@ -8,6 +8,42 @@ describe('jobs requests', () => {
   beforeEach(() => {
     fetchMock.restore();
     Cookies.remove(SESSION_COOKIE);
+  });
+
+  describe('create jobs', () => {
+    it('should send a post to /jobs', async () => {
+      // given
+      Cookies.set(SESSION_COOKIE, 'sometoken');
+      const name = 'some-name';
+      const url = "git@domaine/repo";
+      const key = "some-private-key";
+      const job = {
+        name,
+        gitConfig: {
+          sshAuth: {
+            url,
+            key
+          }
+        }
+      };
+
+      const jobCreationMatcher = (callUrl, opt) => {
+        return (
+          callUrl === "/jobs" &&
+          opt.headers.Authorization === 'Bearer sometoken'  &&
+          JSON.parse(opt.body).name === name &&
+          JSON.parse(opt.body).gitConfig.sshAuth.url === url &&
+          JSON.parse(opt.body).gitConfig.sshAuth.key === key
+        );
+      };
+
+      fetchMock.postOnce(jobCreationMatcher, {
+        status: 201
+      });
+
+      // when
+      await expect(createJob(job)).resolves.toEqual({});
+    });
   });
 
   describe('get jobs', () => {
