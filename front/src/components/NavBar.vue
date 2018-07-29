3480216456f1c46184aef4d513b91fc0488963d1
Jerome Doucet <jerdct@gmail.com>
2018-07-29 21:49:55 +0200

    add the link to RegistrieList component in navBar

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -2,12 +2,15 @@
   <b-navbar id='dahu-navbar' toggleable="md" type="dark" variant="dark">
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
     <b-navbar-brand href="#/">Dahu</b-navbar-brand> 
-    <b-navbar-nav class="ml-auto">
-      <b-nav-form right v-if="isAuthenticated()">
+    <b-navbar-nav v-if="isAuthenticated()">
+      <b-nav-item id="registries-nav-link" href="#/registries">Images registries</b-nav-item>
+    </b-navbar-nav>
+    <b-navbar-nav class="ml-auto" v-if="isAuthenticated()">
+      <b-nav-form right>
         <b-button size="sm" class="my-2 my-sm-0" type="button" v-on:click="disconnect">Disconnect</b-button>
       </b-nav-form>
     </b-navbar-nav>
- </b-navbar>
+  </b-navbar>
 </template>
 
 <script>
--- front/src/routes.js
+++ front/src/routes.js
@@ -2,6 +2,7 @@ import Home from '@/components/Home.vue';
 import JobCreation from '@/components/jobs/JobCreation.vue';
 import JobDetail from '@/components/jobs/JobDetail.vue';
 import Login from '@/components/Login.vue';
+import RegistriesList from '@/components/registries/RegistriesList.vue'
 
 import { isAuthenticated } from '@/services/user';
 
@@ -10,6 +11,7 @@ export const routes = [
   { path: '/', component: Home },
   { path: '/jobs/creation', component: JobCreation },
   { path: '/jobs/:jobId/pipeline', component: JobDetail },
+  { path: '/registries', component: RegistriesList },
 ]
 
 export function checkAuthenticationBeforeNavigation(to, from, next) {
